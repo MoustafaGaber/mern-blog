529613f81487026b3e05287186d34d90d08d0bc9
MoustafaGaber <safi_hp@yahoo.com>
2026-05-17 12:30:46 +0300

    create routes and conotrollers folders

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { connectDB } from "./utils/db.js";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+//routers
+app.use("/api/users", userRoute);
+//for testing
 app.get("/", (req, res) => res.send("Hello World!"));
 
 connectDB()
