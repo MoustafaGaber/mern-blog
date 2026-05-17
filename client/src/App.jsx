a41020babd45fb571904ef7baec90e46bc054484
MoustafaGaber <safi_hp@yahoo.com>
2026-05-17 10:57:28 +0300

    creat header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,19 @@ import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import Signin from './pages/SignIn'
 import Signup from './pages/SignUp'
+import Header from './components/Header'
+import Projects from './pages/Projects'
 const App = () => {
   return (
   <BrowserRouter>
+  <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-up" element={<Signup />} />
     </Routes>
   </BrowserRouter>
   )
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,61 @@
+import { Button, Navbar, TextInput, NavbarCollapse,
+  NavbarLink, NavbarToggle} from "flowbite-react";
+import { Link,useLocation } from "react-router";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+const Header = () => {
+    const path=useLocation().pathname
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-lg font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+          Safi's
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline "
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden " color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button
+            className="bg-gradient-to-r from-purple-500 to-blue-500 text-white hover:bg-gradient-to-l focus:ring-purple-200 dark:focus:ring-purple-800"
+            pill
+          >
+            Sign In
+          </Button>
+        </Link>
+       <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+       <NavbarLink active={path==="/"} as="div"> 
+        <Link to="/">Home</Link>
+        </NavbarLink >
+        <NavbarLink active={path==="/about"} as="div">
+        <Link to="/about">About</Link>
+        </NavbarLink>
+        <NavbarLink active={path==="/projects"} as="div">
+        <Link to="/projects">Projects</Link>
+        </NavbarLink>
+      
+      </NavbarCollapse>
+    </Navbar>
+  );
+};
+
+export default Header;
