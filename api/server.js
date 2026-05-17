d2c0c5a21436c2cc5e780a94af7ef61e66a80957
MoustafaGaber <safi_hp@yahoo.com>
2026-05-17 14:17:31 +0300

    add error handler middleware

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import { connectDB } from "./utils/db.js";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js"
+import { errorHandler } from "./utils/errorHandler.js";
 
 dotenv.config();
 
@@ -15,6 +16,16 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 //for testing
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 
 connectDB()
   .then(() =>
